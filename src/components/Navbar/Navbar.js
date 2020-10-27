5dfb0bb613ec3e9c37a53734afac2d05e3ed091a
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-10-27 11:55:44 +0500

    make it better

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import {IconContext} from 'react-icons/lib';
+import { Nav, 
+         NavbarContainer, 
+         NavLogo, 
+         NavIcon, 
+         MobileIcon,
+         NavMenu,
+         NavItem,
+         NavLinks
+        } from './Navbar.elements';
+const Navbar = () => {
+    const [click, setClick]= useState(false);
+    
+    const handleClick= () => setClick(!click);
+    
+    return (
+        <>
+        <IconContext.Provider value={{color: '#fff'}}>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">
+                        <NavIcon/>
+                            CPaaSelector
+                    </NavLogo>
+                <MobileIcon onClick={handleClick}>
+                    {click ? <FaTimes/> : <FaBars/>}
+                </MobileIcon>
+                <NavMenu onClick={handleClick} click={click}>
+                    <NavItem>
+                        <NavLinks to="/home">
+                            Home
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/services">
+                            Services
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/pricing">
+                            Pricing
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/about">
+                            About
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/login">
+                            Login
+                        </NavLinks>
+                    </NavItem>
+                   </NavMenu>                
+                </NavbarContainer>
+            </Nav>
+        </IconContext.Provider>
+        </>
+    )
+}
+
+export default Navbar
